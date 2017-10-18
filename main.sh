e990d7f4be2ac5019f305674ec41726eb8079c4a
valdz13 <32905302+valdz13@users.noreply.github.com>
2017-10-18 21:53:07 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,86 @@
 #!/bin/sh
+
+#backup storage
+
+##cek_folder ()
+#{
+  #commands
+#}
+
+##################################################
+#Reading source directory and dest directory
+#echo "Masukkan directory yang ingin di-backup: "
+#read DIR
+
+#echo "Masukkan directory tujuan: "
+#read DEST
+##################################################
+
+#file directory that would be backuped
+DIR=$PWD
+
+echo"Masukkan nama direktori tujuan: "
+read DEST
+
+#list all of the files and folders from source directory
+LS=` ls --format=single-column $DIR`
+
+#for date documentation when the files are backuped
+DATE=` date`
+
+
+#checking the directory wether is exist or not
+if [ ! -d $HOME/$DEST ]
+then
+
+	mkdir $HOME/$DEST
+
+fi
+
+#creating logs
+if [ ! -f $HOME/$DEST/log-backup-$DEST ]
+then
+
+	touch $HOME/$DEST/log-backup-$DEST
+
+fi
+
+
+#kalau kosong gimana? kalau folder berisi gimana?
+
+
+
+#Backuping the files
+for FILE in $LS
+
+do
+
+	if [ -f $FILE ] #Jika $FILE merupakan FILE dan bukan folder
+
+	then
+
+		if [ -f $HOME/$DEST/Backup\-$FILE ] #Jika file sudah ada di bakcup folder, hapus dulu
+
+		then
+
+			rm $HOME/$DEST/Backup\-$FILE
+
+		fi
+
+		touch $HOME/$DEST/Backup\-$FILE
+
+		echo "#FILE BACKUP TANGGAL $DATE#" >> $HOME/$DEST/Backup\-$FILE
+
+		cat $HOME/$DIR/$FILE >> $HOME/$DEST/Backup\-$FILE
+
+		echo "$DATE  $USER telah mem-backup file $HOME/$DIR/$FILE" >> $HOME/$DEST/log-backup\-$DEST
+
+		echo -e "Backuping $HOME/$DIR/$FILE" 
+
+	fi
+
+done
+
+
+
+echo -e "====================================" >> $HOME/$DEST/log-backup\-$DEST
