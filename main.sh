be9c19401d22d1171fd2f9591ee9f7bce12b7dc7
Setiawan <setiawan@ti.ukdw.ac.id>
2017-10-20 13:50:45 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,45 +1,32 @@
 #!/bin/sh
-
+#This main.sh will call backup.sh as function
 
 DATE=` date +%d-%m-%Y`
 
 echo "Masukkan folder yang ingin di backup : "
-
 read FOLDER
 
-
-
+#Checking the available backup directory and log file
 if [ ! -d $HOME/Backup ]
-
 then
- 
 	mkdir $HOME/Backup
-
 fi
-
 
 if [ ! -e $PWD/log-backup ]
-
 then
- 
 	touch $PWD/log-backup
-
 fi
+#----------------------------------------------------
 
 
-
+#Starting the backup
 NOW=` date`
-
 DEST="$HOME/Backup"
 
-
 echo  "" >> $PWD/log-backup
-
 echo  "===================== Backup $NOW Started ======================" >> $PWD/log-backup
 
-
 $HOME/./backup.sh $FOLDER $DEST
-
 
 echo  "=================================== END OF BACKUP ====================================" >> $PWD/log-backup
 
@@ -47,14 +34,16 @@ if [ -e backup-$FOLDER-$DATE.tar.gz ]
 then
 	rm backup-$FOLDER-$DATE.tar.gz
 fi
+#----------------------------------------------------
 
+
+#Archiving the backup_directory and delete the backup_directory
 tar -czf backup-$FOLDER-$DATE.tar.gz $HOME/Backup
-
 rm -R $HOME/Backup
+#----------------------------------------------------
 
 
-
+#Backup Finished
 echo
-
 echo "Backup selesai."
-
+#----------------------------------------------------
